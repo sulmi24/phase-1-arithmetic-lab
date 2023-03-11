9970be9457ff601e1b456cad77a8396f6aedae20
sulmi24 <a.sulmi24@gmail.com>
2023-03-11 19:47:16 +0200

    arthumatic  is done

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+
+console.log(multiply);
+
+let random = Math.floor(Math.random() * 2) + 1;
+
+let num3 = 4;
+let num4 = 10;
+
+let mod = num3 % num4;
+
+let max = Math.max(10, 2, 5, 20);
